5510c032e5dce09f7638323d423bf6147615ea40
Alexis Tyler <xo@wvvw.me>
2018-07-13 00:36:31 +0000

    Add routes

--- themes-default/slim/src/router.js
+++ themes-default/slim/src/router.js
@@ -1,18 +1,29 @@
 import VueRouter from 'vue-router';
 import { Login, Config, AddShows, AddRecommended, NotFound } from './components';
 
-const router = new VueRouter({
-    base: document.body.getAttribute('web-root') + '/',
-    mode: 'history',
-    routes: [{
-        path: '/login',
-        name: 'login',
+const homeRoutes = {
+    path: '/home',
+    children: [{
+        path: '',
+        name: 'home',
         meta: {
-            title: 'Login'
-        },
-        component: Login
+            title: 'Home',
+            header: 'Show List'
+        }
     }, {
-        path: '/config',
+        path: 'editShow',
+        name: 'editShow'
+    }, {
+        path: 'displayShow',
+        name: 'displayShow'
+    }]
+};
+
+const configRoutes = {
+    path: '/config',
+    component: { template: '<router-view/>' },
+    children: [{
+        path: '',
         name: 'config',
         meta: {
             title: 'Help & Info',
@@ -20,7 +31,69 @@ const router = new VueRouter({
         },
         component: Config
     }, {
-        path: '/addShows',
+        path: 'anime',
+        name: 'configAnime',
+        meta: {
+            title: 'Config - Anime',
+            header: 'Anime'
+        }
+    }, {
+        path: 'backuprestore',
+        name: 'configBackupRestore',
+        meta: {
+            title: 'Config - Backup/Restore',
+            header: 'Backup/Restore'
+        }
+    }, {
+        path: 'general',
+        name: 'configGeneral',
+        meta: {
+            title: 'Config - General',
+            header: 'General Configuration'
+        }
+    }, {
+        path: 'notifications',
+        name: 'configNotifications',
+        meta: {
+            title: 'Config - Notifications',
+            header: 'Notifications'
+        }
+    }, {
+        path: 'postProcessing',
+        name: 'configPostProcessing',
+        meta: {
+            title: 'Config - Post Processing',
+            header: 'Post Processing'
+        }
+    }, {
+        path: 'providers',
+        name: 'configSearchProviders',
+        meta: {
+            title: 'Config - Providers',
+            header: 'Search Providers'
+        }
+    }, {
+        path: 'search',
+        name: 'configSearchSettings',
+        meta: {
+            title: 'Config - Episode Search',
+            header: 'Search Settings'
+        }
+    }, {
+        path: 'subtitles',
+        name: 'configSubtitles',
+        meta: {
+            title: 'Config - Subtitles',
+            header: 'Subtitles'
+        }
+    }]
+};
+
+const addShowRoutes = {
+    path: '/addShows',
+    component: { template: '<router-view/>' },
+    children: [{
+        path: '',
         name: 'addShows',
         meta: {
             title: 'Add Shows',
@@ -28,43 +101,160 @@ const router = new VueRouter({
         },
         component: AddShows
     }, {
-        path: '/addRecommended',
-        name: 'addRecommended',
+        path: 'addExistingShows',
+        name: 'addExistingShows',
         meta: {
-            title: 'Add Recommended Shows',
-            header: 'Add Recommended Shows'
-        },
-        component: AddRecommended
+            title: 'Add Existing Shows',
+            header: 'Add Existing Shows'
+        }
     }, {
-        path: '/schedule',
-        name: 'schedule',
+        path: 'newShow',
+        name: 'addNewShow',
         meta: {
-            title: 'Schedule',
-            header: 'Schedule'
-        }, // eslint-disable-line comma-dangle
-        // component: scheduleComponent
-    }, {
-        path: '/not-found',
-        name: 'not-found',
-        meta: {
-            title: '404',
-            header: '404 - page not found'
-        },
-        component: NotFound
-    // @NOTE: Redirect can only be added once all routes are vue
-    // }, {
-    //     path: '*',
-    //     redirect: '/not-found'
+            title: 'Add New Show',
+            header: 'Add New Show'
+        }
     }]
+};
+
+const loginRoute = {
+    path: '/login',
+    name: 'login',
+    meta: {
+        title: 'Login'
+    },
+    component: Login
+};
+
+const addRecommendedRoute = {
+    path: '/addRecommended',
+    name: 'addRecommended',
+    meta: {
+        title: 'Add Recommended Shows',
+        header: 'Add Recommended Shows'
+    },
+    component: AddRecommended
+};
+
+const scheduleRoute = {
+    path: '/schedule',
+    name: 'schedule',
+    meta: {
+        title: 'Schedule',
+        header: 'Schedule'
+    }
+};
+
+const historyRoute = {
+    path: '/history',
+    name: 'history',
+    meta: {
+        title: 'History',
+        header: 'History'
+    }
+};
+
+const manageRoutes = {
+    path: '/manage',
+    component: { template: '<router-view/>' },
+    children: [{
+        path: '',
+        name: 'manage',
+        meta: {
+            title: 'Mass Update',
+            header: 'Mass Update'
+        }
+    }, {
+        path: 'backlogOverview',
+        name: 'manageBacklogOverview',
+        meta: {
+            title: 'Backlog Overview',
+            header: 'Backlog Overview'
+        }
+    }, {
+        path: 'episodeStatuses',
+        name: 'manageEpisodeOverview',
+        meta: {
+            title: 'Episode Overview',
+            header: 'Episode Overview'
+        }
+    }, {
+        path: 'failedDownloads',
+        name: 'manageFailedDownloads',
+        meta: {
+            title: 'Failed Downloads',
+            header: 'Failed Downlaods'
+        }
+    }, {
+        path: 'manageSearches',
+        name: 'manageManageSearches',
+        meta: {
+            title: 'Manage Searches',
+            header: 'Manage Searches'
+        }
+    }, {
+        path: 'massEdit',
+        name: 'manageMassEdit',
+        meta: {
+            title: 'Mass Edit'
+        }
+    }, {
+        path: 'subtitleMissed',
+        name: 'manageSubtitleMissed',
+        meta: {
+            title: 'Missing Subtitles',
+            header: 'Missing Subtitles'
+        }
+    }, {
+        path: 'subtitleMissedPP',
+        name: 'manageSubtitleMissedPP',
+        meta: {
+            title: 'Missing Subtitles in Post-Process folder',
+            header: 'Missing Subtitles in Post-Process folder'
+        }
+    }]
+};
+
+const notFoundRoutes = {
+    path: '/not-found',
+    name: 'not-found',
+    meta: {
+        title: '404',
+        header: '404 - page not found'
+    },
+    component: NotFound
+// @NOTE: Redirect can only be added once all routes are vue
+// }, {
+//     path: '*',
+//     redirect: '/not-found'
+};
+
+const routes = [
+    homeRoutes,
+    configRoutes,
+    addShowRoutes,
+    loginRoute,
+    addRecommendedRoute,
+    scheduleRoute,
+    historyRoute,
+    manageRoutes,
+    notFoundRoutes
+];
+
+const router = new VueRouter({
+    base: document.body.getAttribute('web-root') + '/',
+    mode: 'history',
+    routes
 });
 
 router.beforeEach((to, from, next) => {
     const { meta } = to;
     const { title } = meta;
 
-    // If there's no title then it's not a .vue route so skip
+    // If there's no title then it's not a .vue route
+    // or it's handling its own title
     if (title) {
-        document.title = `${to.meta.title} | Medusa`;
+        document.title = `${title} | Medusa`;
     }
 
     // Always call next otherwise the <router-view> will be empty
